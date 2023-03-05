942dc5612255ee6eeafd4903eed05555fd0c7899
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-05 08:54:45 -0500

    add find all to get method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,6 +5,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll().then((tagData) => {
+    res.json(tagData);
+  });
   // be sure to include its associated Product data
 });
 
