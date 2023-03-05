f2e6d563342a23766bea50b59fc9c886e073c079
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-04 23:19:11 -0500

    import and sync sequelize connection on server.js and define columns for all models.

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL, //CHECK
+      allowNull: false,
+      // Validates that the value is a decimal.
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    // Set a default value of 10.
+    // Validates that the value is numeric.
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+    // References the Category model's id.
+    },
   },
   {
     sequelize,
